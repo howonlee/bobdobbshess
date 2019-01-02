aef1353fd9fabd31d3996fa9708fae9b922d592e
howon lee <hleehowon@gmail.com>
2019-01-01 18:13:20 -0800

    define functions...

--- code.py
+++ code.py
@@ -2,6 +2,10 @@ import numpy as np
 import numpy.random as npr
 import numpy.linalg as npl
 
+"""
+fn 1: vector elementwise square
+"""
+
 def fn1(inp):
     pass
 
@@ -12,13 +16,17 @@ def inv_grad1(inp):
     """ functional inverse of the gradient """
     pass
 
-def hess_mult(inp):
+def hess_mult1(inp):
     """ hessian pre-multiplication as in Pearlmutter 1993 """
     pass
 
-def hess_inv__mult(inp):
+def hess_inv_mult1(inp):
     """ inverse hessian pre-multiplication, the novel bit """
     pass
+
+"""
+fn 2: something very mildly more complicated
+"""
 
 if __name__ == "__main__":
     pass
