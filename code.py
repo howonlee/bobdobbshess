8e6bcda126e309cffe8a23da759f02d791cd83d9
howon lee <hleehowon@gmail.com>
2019-01-01 18:10:20 -0800

    add code and start a bit of the writeup

---
+++ code.py
@@ -0,0 +1,24 @@
+import numpy as np
+import numpy.random as npr
+import numpy.linalg as npl
+
+def fn1(inp):
+    pass
+
+def grad1(inp):
+    pass
+
+def inv_grad1(inp):
+    """ functional inverse of the gradient """
+    pass
+
+def hess_mult(inp):
+    """ hessian pre-multiplication as in Pearlmutter 1993 """
+    pass
+
+def hess_inv__mult(inp):
+    """ inverse hessian pre-multiplication, the novel bit """
+    pass
+
+if __name__ == "__main__":
+    pass
